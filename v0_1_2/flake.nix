0d9f9f733794c95f8577e7915cbc1c7941120182
hugosenari <hugosenari@gmail.com>
2022-03-27 18:23:15 -0300

    feat(nimpkgs): remove lock

--- v0_1_2/flake.nix
+++ v0_1_2/flake.nix
@@ -11,6 +11,7 @@
   inputs.src-nimlibxlsxwriter-v0_1_2.ref   = "refs/tags/v0.1.2";
   inputs.src-nimlibxlsxwriter-v0_1_2.owner = "ThomasTJdev";
   inputs.src-nimlibxlsxwriter-v0_1_2.repo  = "nimlibxlsxwriter";
+  inputs.src-nimlibxlsxwriter-v0_1_2.dir   = "";
   inputs.src-nimlibxlsxwriter-v0_1_2.type  = "github";
   
   inputs."nimgen".owner = "nim-nix-pkgs";
